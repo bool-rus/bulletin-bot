dce99509b359edd0e083060081e1238b758b086f
Serge Barinov <bool.rus@gmail.com>
2022-05-28 20:00:53 +0300

    lbit fix

--- src/bots/bulletin/impls.rs
+++ src/bots/bulletin/impls.rs
@@ -6,7 +6,8 @@ use teloxide::utils::markdown::*;
 fn make_ad_text(user: &User, ad: &Ad) -> String {
     let text = escape(&ad.text);
     let price = bold(&format!("{} ₽", ad.price));
-    let sign = user_mention(user.id.0.try_into().unwrap(), &user.full_name());
+    let full_name = escape(&user.full_name());
+    let sign = user_mention(user.id.0.try_into().unwrap(), &full_name);
     format!("{}\n\n{}\n{}\n", text, price, sign)
 }
 
